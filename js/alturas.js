366c47212026e14f36e380af30671afd2e2cdb1d
JosemaGonzalez <josemagp94@gmail.com>
2017-05-10 12:36:20 +0200

    10052017

--- js/alturas.js
+++ js/alturas.js
@@ -13,6 +13,13 @@ $(document).ready(function() {
     let ventana_centro = $(window).width()/20;
     $(".centro").css("right", ventana_centro);
     $(".centro").css("left", ventana_centro);
+    $("#sobremi .drcha img").css("width", "18%");
+    let img= $("#sobremi .drcha img").width();
+    let img2=  $("#sobremi .drcha img").height();
+    $(".pro svg").css("width", img);
+    $(".pro svg").css("height",img2);
+    $(".drcha .sobredrcha,.drcha .sobreizda").css("margin-top", "2px");
+
     /*Dispositivos pequeños*/
     if ($(window).height() < "550") {
         $("#sobremi .izda,#sobremi .drcha").css("margin","1px auto");
@@ -27,23 +34,13 @@ $(document).ready(function() {
 
     }
     if ($(window).height() < "550" || $(window).width() < "350") {
-        $(".apt img").css("margin", "0");
-        $(".apt div").css("max-width", "45px");
-        $(".apt div").css("max-height", "45px");
-        $(".apt div").css("top", "0");
         $("#sobremi h4").css("margin-bottom", "1%");
-        $(".apt").css("margin-top", "2%");
         $(".centro2").css("margin-top", "5%");
-        $(".apt").css("width", "24%");
         $(".pro span").css("font-size", "0.8em");
-        $(".pro span").css("top", "9%");
-
-    }else{
-        $(".pro span").css("top", "15%");
     }
     /*Tablet*/
     if ($(window).width() >= "768") {
-        $("#sobremi .izda,#sobremi .drcha").height(ventana_alto/3);
+        $("#sobremi .izda,#sobremi .drcha").height(ventana_alto/2.8);
         $(".sobreizda,.sobredrcha").css("max-height", ventana_alto2/3.2);
         $(".sobreizda,.sobredrcha").css("min-height", ventana_alto2/3.2);
         $("#mistrabajos .izda a,#mistrabajos .drcha a").height(ventana_alto2/2.7);
@@ -54,11 +51,8 @@ $(document).ready(function() {
         $(".sobreizda,.sobredrcha").css("max-height", ventana_alto2/2.5);
         $(".sobreizda,.sobredrcha").css("min-height", ventana_alto2/2.5);
         $("#sobremi .izda,#sobremi .drcha").height(ventana_alto/2);
+        $("#sobremi .izda,#sobremi .drcha").css("margin-top","5%");
         $("#mistrabajos .izda a,#mistrabajos .drcha a").height(ventana_alto2/3);
 
-    }
-    /*PC grande*/
-    if ($(window).width() >= "1400") {
-        $("#sobremi .izda,#sobremi .drcha").height(ventana_alto/2.4);
     }
 });
